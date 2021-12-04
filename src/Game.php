5e05b70e7b5966f944f96c955eea8f1ea838c436
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2021-12-04 16:27:25 +0200

    added score counting function, 1st and 3rd test passes

--- src/Game.php
+++ src/Game.php
@@ -5,11 +5,28 @@ namespace App;
 class Game
 {
 	const FRAMES_PER_GAME = 10;
-	protected $rolls = [];
+	private $rolls = [];
 	
 	// store the rolls in the rolls array
 	public function roll($pins)
 	{
 		$this->rolls[] = $pins;
+	}
+
+	// counts the score of the game
+	public function score()
+	{
+		$score = 0;
+		$rollIndex = 0;
+		for ($frame = 0; $frame < self::FRAMES_PER_GAME; $frame++) {
+			$score += $this->sumOfBallsInFrame($rollIndex);
+			$rollIndex += 2;
+		}
+		return $score;
+	}
+
+	private function sumOfBallsInFrame($rollIndex)
+	{
+		return $this->rolls[$rollIndex] + $this->rolls[$rollIndex + 1];
 	}
 }
