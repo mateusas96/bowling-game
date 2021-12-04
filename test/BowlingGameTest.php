7cfb6eece2d43de58843e6bd569720ed50d2c1d7
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2021-12-04 16:12:34 +0200

    added one more test, fixed bugs in test class

--- test/BowlingGameTest.php
+++ test/BowlingGameTest.php
@@ -44,14 +44,17 @@ class BowlingGameTest extends TestCase
 		$this->assertEquals(20, $this->game->score());
 	}
 
-	public function testAllSpareButAdditionalFrameIsStrike()
+	// test all spare but last frame is all strikes
+	public function testAllSpareButLastFrameIsPerfect()
 	{
 	
-		foreach (range(1, 18) as $roll) {
+		foreach (range(1, 9) as $roll) {
 			$this->game->roll(5);
 			$this->game->roll(5);
 		}
 		
+		$this->game->roll(10);
+		$this->game->roll(10);
 		$this->game->roll(10);
 		
 		// TODO: need to count the score, so just added a random number
@@ -78,7 +81,7 @@ class BowlingGameTest extends TestCase
 			$this->game->roll(0);
 		}
 
-		foreach (range(1, 5) as $roll) {
+		foreach (range(1, 7) as $roll) {
 			$this->game->roll(10);
 		}
 
@@ -88,13 +91,13 @@ class BowlingGameTest extends TestCase
 
 	public function testAllMissesButOneFrameIsStrike()
 	{
-		foreach (range(1, 9) as $roll) {
+		foreach (range(1, 10) as $roll) {
 			$this->game->roll(0);
 		}
 
 		$this->game->roll(10);
 
-		foreach (range(1, 9) as $roll) {
+		foreach (range(1, 8) as $roll) {
 			$this->game->roll(0);
 		}
 
@@ -103,17 +106,31 @@ class BowlingGameTest extends TestCase
 
 	public function testEverySecondFrameIsStrike()
 	{
-		foreach (range(1, 17) as $roll) {
-			if ($roll % 2 == 0) {
+		foreach (range(1, 15) as $roll) {
+			if ($roll % 3 == 0) {
 				$this->game->roll(10);
 			} else {
 				$this->game->roll(0);
 			}
 		}
 
+		$this->game->roll(10);
+		$this->game->roll(10);
+
 		// TODO: need to count the score, so just added a random number
 		// 00 x 00 x 00 x 00 x 00 xxx
 		$this->assertEquals(9999, $this->game->score());
+	}
+
+	// test all frames are spares
+	public function testAllSpares()
+	{
+		foreach (range(1, 21) as $roll) {
+			$this->game->roll(5);
+		}
+
+		// TODO: need to count the score, so just added a random number
+		$this->assertEquals(150, $this->game->score());
 	}
 }
 
